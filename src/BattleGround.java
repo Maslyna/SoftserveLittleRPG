904d3dbbee5672ba7490a764decf43446dee879c
Maslyna <sanci324@gmail.com>
2022-10-25 20:43:45 +0200

    Start of develop.

---
+++ src/BattleGround.java
@@ -0,0 +1,11 @@
+import java.util.*;
+
+
+public class BattleGround {
+
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+        Scanner scan = new Scanner(System.in);
+    }
+
+}
